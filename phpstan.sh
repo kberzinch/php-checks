cc703c5a02afe24821a429b9eb9049f4ed55b8a4
Kristaps Berzinch <kristapsberzinch@gmail.com>
2018-12-25 13:42:58 -0600

    Fix PHPStan failure

--- phpstan.sh
+++ phpstan.sh
@@ -3,7 +3,7 @@
 cd /var/tmp/php-checks/$1
 
 if [ ! -f "phpstan.neon" ]; then
-$(dirname "$0")/vendor/bin/phpstan analyse --no-progress --configuration=/var/www/php-checks/default-phpstan.neon --level=max --error-format=checkstyle .
+$(dirname "$0")/vendor/bin/phpstan analyse --no-progress --configuration=$(dirname "$0")/default-phpstan.neon --level=max --error-format=checkstyle .
 else
 $(dirname "$0")/vendor/bin/phpstan analyse --no-progress --level=max --error-format=checkstyle .
 fi
