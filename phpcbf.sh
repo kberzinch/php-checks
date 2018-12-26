00bf6137661d1b113f4eadde1cb10d63215a7350
Kristaps Berzinch <kristapsberzinch@gmail.com>
2018-12-26 12:41:13 -0600

    Don't exit if phpcbf returns nonzero

--- phpcbf.sh
+++ phpcbf.sh
@@ -4,7 +4,7 @@ cd /var/tmp/php-checks/$1
 
 git checkout $2
 
-$(dirname "$0")/vendor/bin/phpcbf --ignore=vendor --standard=psr2 .
+! $(dirname "$0")/vendor/bin/phpcbf --ignore=vendor --standard=psr2 .
 
 git add .
 git commit -m "Fix style issues"
