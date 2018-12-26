7db923ffa6b299d33584ad35a634579407c07b80
Kristaps Berzinch <kristapsberzinch@gmail.com>
2018-12-26 16:03:43 -0600

    Typo

--- phpcbf.sh
+++ phpcbf.sh
@@ -5,7 +5,8 @@ cd /var/tmp/php-checks/$1
 git checkout $3
 git pull
 
-if [[ "$2" -ne "$(git rev-parse HEAD)" ]] then
+if [[ "$2" -ne "$(git rev-parse HEAD)" ]]
+then
     echo "Branch head does not match requested commit, exiting."
     exit;
 fi
