69faa9c149a9f2388dcbb882ba187aa9d6118c72
Kristaps Berzinch <kristapsberzinch@gmail.com>
2018-12-28 13:29:21 -0600

    Add several packed plugins to phan analysis

--- phan.sh
+++ phan.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-../vendor/bin/phan --exclude-directory-list /var/tmp/php-checks/$1/vendor --output-mode json --output $2 --directory /var/tmp/php-checks/$1 --dead-code-detection --strict-type-checking
+../vendor/bin/phan --exclude-directory-list /var/tmp/php-checks/$1/vendor --output-mode json --output $2 --directory /var/tmp/php-checks/$1 --dead-code-detection --strict-type-checking --plugin DollarDollarPlugin --plugin AlwaysReturnPlugin --plugin DuplicateArrayKeyPlugin --plugin PregRegexCheckerPlugin --plugin PrintfCheckerPlugin --plugin UnreachableCodePlugin --plugin NonBoolBranchPlugin --plugin NonBoolInLogicalArithPlugin --plugin DuplicateExpressionPlugin
