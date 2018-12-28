981f133e463c6bc31214062426fc14f5ce55f49f
Kristaps Berzinch <kristapsberzinch@gmail.com>
2018-12-28 16:46:18 -0600

    Do not bundle any configuration if user-provided xml present

--- messdetector.sh
+++ messdetector.sh
@@ -3,7 +3,7 @@
 cd $(dirname "$0")/workspace/$1
 
 if [ -f "phpmd.xml" ]; then
-$(dirname "$0")/vendor/bin/phpmd . xml cleancode,design,naming,unusedcode,phpmd.xml --ignore-violations-on-exit --exclude vendor --reportfile $2
+$(dirname "$0")/vendor/bin/phpmd . xml phpmd.xml --ignore-violations-on-exit --exclude vendor --reportfile $2
 else
 $(dirname "$0")/vendor/bin/phpmd . xml cleancode,design,naming,unusedcode --ignore-violations-on-exit --exclude vendor --reportfile $2
 fi
