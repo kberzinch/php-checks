a54fbd7755fb9ebe3eec4bafdc2cca40fee3c368
Kristaps Berzinch <kristapsberzinch@gmail.com>
2018-12-28 16:36:47 -0600

    Add support for custom phpmd.xml

--- messdetector.sh
+++ messdetector.sh
@@ -2,4 +2,8 @@
 
 cd $(dirname "$0")/workspace/$1
 
+if [ -f "phpmd.xml" ]; then
+$(dirname "$0")/vendor/bin/phpmd . xml cleancode,design,naming,unusedcode,phpmd.xml --ignore-violations-on-exit --exclude vendor --reportfile $2
+else
 $(dirname "$0")/vendor/bin/phpmd . xml cleancode,design,naming,unusedcode --ignore-violations-on-exit --exclude vendor --reportfile $2
+fi
