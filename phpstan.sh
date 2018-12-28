8f76c0d62e833895f2ede7a3c5e046495bdfe7b6
Kristaps Berzinch <kristapsberzinch@gmail.com>
2018-12-28 14:00:33 -0600

    Strip config file note from phpstan

--- phpstan.sh
+++ phpstan.sh
@@ -3,7 +3,7 @@
 cd /var/tmp/php-checks/$1
 
 if [ ! -f "phpstan.neon" ]; then
-$(dirname "$0")/vendor/bin/phpstan analyse --no-progress --configuration=$(dirname "$0")/default-phpstan.neon --level=max --error-format=checkstyle .
+$(dirname "$0")/vendor/bin/phpstan analyse --no-progress --configuration=$(dirname "$0")/default-phpstan.neon --level=max --error-format=checkstyle . | grep -v "Note: Using configuration file /var/tmp/php-checks/${1}/phpstan.neon."
 else
 $(dirname "$0")/vendor/bin/phpstan analyse --no-progress --level=max --error-format=checkstyle .
 fi
