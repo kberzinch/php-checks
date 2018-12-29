15aa09d55d836601793bdd07b0458f0e516c888e
Kristaps Berzinch <kristapsberzinch@gmail.com>
2018-12-29 14:17:26 -0600

    Add support for user-provided phan config

--- phan.sh
+++ phan.sh
@@ -2,4 +2,8 @@
 
 cd $(dirname "$0")/workspace/$1
 
+if [ -f ".phan/config.php" ]; then
+$(dirname "$0")/vendor/bin/phan
+else
 $(dirname "$0")/vendor/bin/phan --exclude-directory-list vendor --output-mode json --output $2 --directory . --dead-code-detection --strict-type-checking --plugin DollarDollarPlugin --plugin AlwaysReturnPlugin --plugin DuplicateArrayKeyPlugin --plugin PregRegexCheckerPlugin --plugin PrintfCheckerPlugin --plugin UnreachableCodePlugin --plugin NonBoolBranchPlugin --plugin NonBoolInLogicalArithPlugin --plugin DuplicateExpressionPlugin --plugin UnusedSuppressionPlugin
+fi
