c17c63e7e5934df3e5a42472056793df7314209d
Kristaps Berzinch <kristapsberzinch@gmail.com>
2019-03-09 16:40:16 -0500

    +ignore bootstrap/cache for laravel

--- codesniffer.sh
+++ codesniffer.sh
@@ -4,4 +4,4 @@ cd $(dirname "$0")/workspace/$1
 
 $(dirname "$0")/vendor/bin/phpcs --config-set ignore_errors_on_exit 1
 $(dirname "$0")/vendor/bin/phpcs --config-set ignore_warnings_on_exit 1
-$(dirname "$0")/vendor/bin/phpcs --ignore=vendor,database,*.min.js,*.blade.php --standard=psr2 --report=json --report-file=$2 .
+$(dirname "$0")/vendor/bin/phpcs --ignore=vendor,database,*.min.js,*.blade.php,bootstrap/cache --standard=psr2 --report=json --report-file=$2 .
