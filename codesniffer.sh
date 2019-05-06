b2409c2946c81e94132337fcb7feebcdc295b291
Kristaps Berzinch <kristapsberzinch@gmail.com>
2019-05-06 17:53:34 -0500

    Allow CodeSniffer config to be adjusted at runtime

--- codesniffer.sh
+++ codesniffer.sh
@@ -4,4 +4,5 @@ cd $(dirname "$0")/workspace/$1
 
 $(dirname "$0")/vendor/bin/phpcs --config-set ignore_errors_on_exit 1
 $(dirname "$0")/vendor/bin/phpcs --config-set ignore_warnings_on_exit 1
-$(dirname "$0")/vendor/bin/phpcs --ignore=vendor,database,*.min.js,*.blade.php,bootstrap/cache --standard=psr2 --report=json --report-file=$2 .
+$(dirname "$0")/vendor/bin/phpcs --config-set default_standard psr2
+$(dirname "$0")/vendor/bin/phpcs --ignore=vendor,database,*.min.js,*.blade.php,bootstrap/cache --report=json --report-file=$2 .
