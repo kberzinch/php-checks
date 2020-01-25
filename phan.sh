0b938b4d9605d4186f4671b91f489b21503d9aac
Kristaps Berzinch <kristapsberzinch@gmail.com>
2020-01-25 11:08:08 -0500

    Add additional flags to Phan defaults

--- phan.sh
+++ phan.sh
@@ -5,5 +5,5 @@ cd $(dirname "$0")/workspace/$1
 if [ -f ".phan/config.php" ]; then
 $(dirname "$0")/vendor/bin/phan --output-mode json --output $2
 else
-$(dirname "$0")/vendor/bin/phan --exclude-directory-list vendor --output-mode json --output $2 --directory . --dead-code-detection --strict-type-checking --plugin DollarDollarPlugin --plugin AlwaysReturnPlugin --plugin DuplicateArrayKeyPlugin --plugin PregRegexCheckerPlugin --plugin PrintfCheckerPlugin --plugin UnreachableCodePlugin --plugin NonBoolBranchPlugin --plugin NonBoolInLogicalArithPlugin --plugin DuplicateExpressionPlugin --plugin UnusedSuppressionPlugin
+$(dirname "$0")/vendor/bin/phan --exclude-directory-list vendor --output-mode json --output $2 --directory . --signature-compatibility --redundant-condition-detection --dead-code-detection --strict-type-checking --plugin DollarDollarPlugin --plugin AlwaysReturnPlugin --plugin DuplicateArrayKeyPlugin --plugin PregRegexCheckerPlugin --plugin PrintfCheckerPlugin --plugin UnreachableCodePlugin --plugin NonBoolBranchPlugin --plugin NonBoolInLogicalArithPlugin --plugin DuplicateExpressionPlugin --plugin UnusedSuppressionPlugin
 fi
