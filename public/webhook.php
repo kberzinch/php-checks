a4ce7e7fd447e2a65584669435a70101dc0f69a5
Kristaps Berzinch <kristapsberzinch@gmail.com>
2020-02-15 14:50:49 -0500

    Add support for new phpstan config problem

--- public/webhook.php
+++ public/webhook.php
@@ -598,27 +598,35 @@ switch ($_SERVER['HTTP_X_GITHUB_EVENT']) {
                                         $matches
                                     )
                                 ) {
-                                    github(
-                                        $payload['check_run']['url'],
-                                        [
-                                            'conclusion' => 'action_required',
-                                            'completed_at' => date(DATE_ATOM),
-                                            'details_url' => $plain_log_url,
-                                            'output' => [
-                                                'title' => 'Could not parse PHPStan output',
-                                                'summary' => 'Please send the output to the check developer.',
+                                    if (
+                                        1 !== preg_match(
+                                            '/Ignored error (.+) has an unescaped anchor .+ in the middle\. This leads to unintended behavior\. Use .+ instead\./',
+                                            trim($violation['message']->__toString()),
+                                            $matches
+                                        )
+                                    ) {
+                                        github(
+                                            $payload['check_run']['url'],
+                                            [
+                                                'conclusion' => 'action_required',
+                                                'completed_at' => date(DATE_ATOM),
+                                                'details_url' => $plain_log_url,
+                                                'output' => [
+                                                    'title' => 'Could not parse PHPStan output',
+                                                    'summary' => 'Please send the output to the check developer.',
+                                                ],
                                             ],
-                                        ],
-                                        'reporting check_run failure',
-                                        'application/vnd.github.antiope-preview+json',
-                                        'PATCH',
-                                        200
-                                    );
-                                    http_response_code(500);
-                                    exit(
-                                        'Could not parse output from PHPStan '
-                                            . trim($violation['message']->__toString())
-                                    );
+                                            'reporting check_run failure',
+                                            'application/vnd.github.antiope-preview+json',
+                                            'PATCH',
+                                            200
+                                        );
+                                        http_response_code(500);
+                                        exit(
+                                            'Could not parse output from PHPStan '
+                                                . trim($violation['message']->__toString())
+                                        );
+                                    }
                                 }
                             }
 
